846d1415b47b77a738e18996f2441e48b938e998
arun-ramesh2 <arun.ramesh2@hotmail.com>
2023-06-22 16:16:55 +0530

    Add files via upload

---
+++ springboot-demo-hello-world/src/test/java/com/example/springbootdemohelloworld/SpringbootDemoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
